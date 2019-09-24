6b74393d5a7442b80281a33c9857c2d2bb84464d
gw79 <gw79@duke.edu>
2019-09-24 08:19:33 -0400

    asd

---
+++ 048_tests_rot_matrix/gene_test.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  
+  for(int i = 0; i < 10; ++i){
+    for(int j = 0; j < 10; ++j){
+      printf("%c",255);
+    }
+    printf("\n");
+  }
+  return 0;
+  
+}
