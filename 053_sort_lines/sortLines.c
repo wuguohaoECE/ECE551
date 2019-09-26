d471e206ce402007150106e05db82c2fea5de98e
gw79 <gw79@duke.edu>
2019-09-26 05:53:48 -0400

    asd

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -16,9 +16,50 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+
+char ** read_file(FILE * f, size_t * strings_size){
+  char ** p = malloc( (*strings_size+1) * sizeof( *p ) );
+  p[*strings_size]=NULL;
+  size_t n = 0;
+  while( getline( &p[*strings_size], &n, f )>=0 ){
+    (*strings_size)++;
+    p = realloc( p, (*strings_size+1)*sizeof(*p) );
+    p[*strings_size]=NULL;
+    n=0;
+  }
+  sortData(p, *strings_size);
+  return p;
+}
+    
+
+void print_char(char ** array, size_t strings_size){
+  for(int j = 0; j < strings_size; j++){
+      printf("%s", array[j]);
+      free(array[j]);
+    }
+  free(array);
+}
+
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
+  size_t strings_size = 0;
+  char ** array;
+  if(argc==1){
+    array = read_file(stdin, &strings_size);
+    print_char(array, strings_size);
+  }
+  if(argc>1){
+    FILE * f;
+    for(int i = 1; i < argc; i++){
+      strings_size = 0;
+      f = fopen(argv[i], "r");
+      if( f == NULL ){
+        perror("The error was");
+	return EXIT_FAILURE;
+      }
+      array = read_file(f, &strings_size);
+      print_char(array, strings_size);
+    }
+  }
   return EXIT_SUCCESS;
 }
+      
