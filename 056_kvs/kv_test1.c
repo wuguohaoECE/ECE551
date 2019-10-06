cde66d1de39854fb2b7b1a440a4e69fd9bfe4ab6
gw79 <gw79@duke.edu>
2019-10-06 03:43:18 -0400

    asd

--- 056_kvs/kv_test1.c
+++ 056_kvs/kv_test1.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "kv.h"
 
+struct _kvpair_t {
+  char * key;
+  char * value;
+  //DEFINE ME!
 
-    
+};
+typedef struct _kvpair_t kvpair_t;
+
+struct _kvarray_t {
+  kvpair_t * kvarr;
+  size_t len;
+  //DEFINE ME!
+};
+typedef struct _kvarray_t kvarray_t;
+
+typedef struct {
+  char ** s;
+  size_t len;
+}char_t;
 	
 char_t read_file(FILE * f){
   char_t ans;
@@ -17,7 +33,6 @@ char_t read_file(FILE * f){
     ans.s = realloc(ans.s, ans.len * sizeof(*ans.s));
     ans.s[ans.len-1] = NULL;
   }
-  free(ans.s[ans.len-1]);
   ans.len--;
   return ans;
 }
@@ -41,41 +56,20 @@ kvarray_t * readKVs(const char * fname) {
   char_t file_char = read_file(f);
   kvarray_t * kv = malloc(sizeof(*kv));
   kv->len = file_char.len;
-  //printf("%ld\n%ld\n",kv->len,sizeof(*kv->kvarr));
+  printf("%d\n%d\n",kv->len,sizeof(*kv->kvarr));
   kv->kvarr = malloc(kv->len * sizeof(*kv->kvarr));
   for(int i = 0; i < kv->len; i++){
     break_kv(&(kv->kvarr[i]), file_char.s[i]);
-    // printf("key = %s value = %s\n",(kv->kvarr)[i].key,file_char.s[i]);
+    //printf("key = %d value = %s\n",(kv->kvarr)[i].key,file_char.s[i]);
   }
-  free(file_char.s);
-  fclose(f);
   return kv;
 }
 
-void freeKVs(kvarray_t * pairs) {
-  for(int i = 0; i < pairs->len; i++){
-    free(pairs->kvarr[i].key);
+int main(void){
+  kvarray_t * ans = readKVs("test.txt");
+  printf("%ld\n",ans->len);
+  for(int i = 0; i < ans->len; i++){
+    printf("key = %s value = %s\n",ans->kvarr[i].key , ans->kvarr[i].value);
   }
-  free(pairs->kvarr);
-  free(pairs);
-  //WRITE ME
-}
-
-void printKVs(kvarray_t * pairs) {
-  for(int i = 0; i < pairs->len; i++){
-    printf("key = '%s' value = '%s'\n", pairs->kvarr[i].key, pairs->kvarr[i].value);
-  }
-  //WRITE ME
-}
-
-char * lookupValue(kvarray_t * pairs, const char * key) {
-  //WRITE ME
-  char * ans = NULL;
-  for(int i = 0; i < pairs->len; i++){
-    if( strcmp(pairs->kvarr[i].key, key) == 0 ){
-      ans = pairs->kvarr[i].value;
-      break;
-    }
-  }
-  return ans;
+  return EXIT_SUCCESS;
 }
