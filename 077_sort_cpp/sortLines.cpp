cd4d8a658a1a583af166353eca3e2ab7e042a030
gw79 <gw79@duke.edu>
2019-10-24 12:53:56 -0400

    s

--- 077_sort_cpp/sortLines.cpp
+++ 077_sort_cpp/sortLines.cpp
@@ -3,11 +3,39 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int main(int argc, char ** argv){
-  for(int i = 1; i < argc; i++){
-    ifstream f(argv[i], fstream::in);
-    if( !f.is_open() ){
-      cout << "open file failed" <<endl;
+void printSort(istream& ifs) {
+  vector<string> Vec;
+  while (1){
+    string str;
+    getline(ifs,str);
+    if ( ifs.eof() ){
+      break;
     }
+    Vec.push_back(str);
+  }
+  sort(Vec.begin(), Vec.end());
+  for(size_t i = 0; i < Vec.size(); i++){
+    cout << Vec[i] << "\n";
+  }
+}
+int main(int argc, char ** argv){
+  if (argc < 2) {
+    printSort(cin);
+  }
+  else {
+    for(int i = 1; i < argc; i++){
+      ifstream ifs;
+      ifs.open (argv[i], ifstream::in);
+      if ( !ifs.is_open() ){
+	cout << "Error opening file" << endl;
+	return EXIT_FAILURE;
+      }
+      printSort(ifs);
+    }
+  }
+  return EXIT_SUCCESS;
+}
     
