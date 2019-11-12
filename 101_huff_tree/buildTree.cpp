be815fad0b1f0aece90e92c764f531bb5da4ce6c
gw79 <gw79@duke.edu>
2019-11-12 13:08:38 -0500

    s

--- 101_huff_tree/buildTree.cpp
+++ 101_huff_tree/buildTree.cpp
@@ -3,7 +3,9 @@
 Node *  buildTree(uint64_t * counts) {
   priority_queue_t pq;
   for(uint64_t i = 0; i < 257; i++) {
-    pq.push(new Node(i,counts[i]));
+    if (counts[i] != 0) {
+      pq.push(new Node(i,counts[i]));
+    }
   }
   while(pq.size() > 1) {
     Node * tmp1 = pq.top();
