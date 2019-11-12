da6ecd159b31dcb51a5c9b81fb0e1d56a9aeff96
gw79 <gw79@duke.edu>
2019-11-12 14:23:44 -0500

    s

--- 102_huff_map/buildTree.cpp
+++ 102_huff_map/buildTree.cpp
@@ -1,1 +1,19 @@
-../100_huff_tree/buildTree.cpp
+#include "node.h"
+
+Node *  buildTree(uint64_t * counts) {
+  priority_queue_t pq;
+  for(uint64_t i = 0; i < 257; i++) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i,counts[i]));
+    }
+  }
+  while(pq.size() > 1) {
+    Node * tmp1 = pq.top();
+    pq.pop();
+    Node * tmp2 = pq.top();
+    pq.pop();
+    pq.push(new Node(tmp1,tmp2));
+  }
+  return pq.top();
+  //WRITE ME!
+}  
--- 102_huff_map/node.cpp
+++ 102_huff_map/node.cpp
@@ -1,5 +1,15 @@
+
 #include "node.h"
 void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
   //WRITE ME!
+  if(left != NULL) {
+    left->buildMap(b.plusZero(), theMap);
+  }
+  if(right != NULL) {
+    right->buildMap(b.plusOne(), theMap);
+  }
+  if(sym != 999) {
+    theMap[sym]= b;
+  }
 }
 
