fca1c93ff3adeba9dca5c086b8f6e415ea44a837
gw79 <gw79@duke.edu>
2019-11-12 13:03:41 -0500

    s

--- 101_huff_tree/buildTree.cpp
+++ 101_huff_tree/buildTree.cpp
@@ -1,5 +1,17 @@
 #include "node.h"
 
 Node *  buildTree(uint64_t * counts) {
+  priority_queue_t pq;
+  for(uint64_t i = 0; i < 257; i++) {
+    pq.push(new Node(i,counts[i]));
+  }
+  while(pq.size() > 1) {
+    Node * tmp1 = pq.top();
+    pq.pop();
+    Node * tmp2 = pq.top();
+    pq.pop();
+    pq.push(new Node(tmp1,tmp2));
+  }
+  return pq.top();
   //WRITE ME!
 }  
--- 101_huff_tree/readFreq.cpp
+++ 101_huff_tree/readFreq.cpp
@@ -1,1 +1,56 @@
-../099_huff_freq/readFreq.cpp
+#include <stdio.h>
+#include <cstdlib>
+#include "readFreq.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+void printSym(std::ostream & s, unsigned sym) {
+  if (sym > 256 ) {
+    s << "INV";
+  }
+  else if (sym == 256) {
+    s << "EOF";
+  }
+  else if (isprint(sym)) {
+    char c = sym;
+    s << "'" << c << "'";
+  }
+  else {
+    std::streamsize w = s.width(3);
+    s << std::hex << sym << std::dec;
+    s.width(w);
+  }
+}
+uint64_t * readFrequencies(const char * fname) {
+  //WRITE ME!
+  /*
+  ifstream in(fname);
+  string contents;
+  if (in) {
+    in.seekg(0, ios::end);
+    size_t len = in.tellg();
+    in.seekg(0);
+    contents.resize(len + 1, '\0');
+    in.read(&contents[0], len);
+  }
+  */
+  FILE * f = fopen(fname, "r");
+  if (f == NULL) {
+    perror("cant open file");
+  }
+  uint64_t * count = new uint64_t[257]();
+  count[256] = 1;
+  int c;
+  while ((c = fgetc(f)) != EOF) {
+    count[c]++;
+  }
+  fclose(f);
+  /*
+  for (auto it = contents.begin(); it != contents.end(); ++it) {
+    count[(uint64_t)*it]++;
+  }
+  */
+  return count;
+}
+
